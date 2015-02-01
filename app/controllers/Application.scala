ef256cbb54d6f53a33cb0e8b74b6aaafc36ef967
anatolyr <anatolyr@wix.com>
2015-02-01 21:30:28 +0200

    Debug log

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -15,6 +15,11 @@ object Application extends Controller {
   }
 
   def addEvent(userName: String) = Action { request =>
+
+    println("------------------ Request Json ------------------------")
+    println(request.body.asJson)
+    println("--------------------------------------------------------\n")
+
     val event = request.body.asJson.get.as[Event]
 
     eventManager.addEvent(userName, event)
